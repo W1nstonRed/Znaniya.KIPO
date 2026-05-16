bfe79b0b609a2ebe94a1baab509ed9dd3161f2df
root <root@fawn-starlite.mhost.ee>
2026-05-16 15:45:24 +0200

    add ci/cd

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<h1 class="text-red text-bold text-2xl">v 0.0.1</h1>
