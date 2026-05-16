9e8b9c3876839370245323a5f0d82b6989ed18f6
W1nstonRed <143222797+W1nstonRed@users.noreply.github.com>
2026-05-16 16:57:35 +0300

    test ci/cd

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,3 +2,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 
 <h1 class="text-red text-bold text-2xl">v 0.0.1</h1>
+<h2 class="bg-black text-white text-2xl">CI/CD feature</h2>
