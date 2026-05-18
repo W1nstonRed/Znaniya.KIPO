e1a6cd892f90258b678348466e0cd810d72d2f62
W1nstonRed <143222797+W1nstonRed@users.noreply.github.com>
2026-05-18 10:47:07 +0300

    Update config.ts

--- frontend/src/lib/api/client/config.ts
+++ frontend/src/lib/api/client/config.ts
@@ -1,5 +1,9 @@
 import { PUBLIC_API_BASE_URL } from '$env/static/public'
 
+const isServer = typeof window === 'undefined'
+
 export const config = {
-    apiBaseUrl: PUBLIC_API_BASE_URL || 'http://localhost:3000/api',
+    apiBaseUrl: isServer
+        ? 'http://localhost:3000' // SSR — напрямую к бэкенду
+        : PUBLIC_API_BASE_URL || 'http://localhost:3000/api', // браузер — через nginx
 }
